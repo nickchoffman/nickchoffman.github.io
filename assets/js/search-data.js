515503acc750faf57ba540218a3d94378df0f72c
nickchoffman <54482668+nickchoffman@users.noreply.github.com>
2026-05-27 14:45:32 +0000

    Deploying to gh-pages from @ nickchoffman/nickchoffman.github.io@01b3c9190d756ae764cd2df17a0ee8abc9d77187 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-research",
-          title: "Research",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/publications/";
-          },
-        },{id: "post-a-post-with-plotly-js",
+  },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
